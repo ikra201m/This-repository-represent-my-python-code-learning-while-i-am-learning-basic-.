b4d4977a97a4b38ab19fae2e1c5fc19ee82f06fe
IkramMathDu <141448514+ikra201m@users.noreply.github.com>
2026-01-04 11:46:51 +0600

    Add files via upload

---
+++ ClassFive.py
@@ -0,0 +1,179 @@
+# #loops in python
+# # #while
+# # count=1
+# # while count <=5:
+# #     print("hello")
+# #     count=count+1
+    
+# # print(count)
+
+# # i=1
+# # while i <=100:
+# #     print("i am boss")
+# #     i=i+1
+
+# # print(i)
+# #print number 1 to 5
+# # i=1
+# # while i <=5:
+# #     print(i)
+# #     i=i+1
+
+
+# # i=5
+# # while i >=1:
+# #     print(i)
+# #     i=i-1
+
+# #print number from 1 to 100
+# # i=1
+# # while i<=100:
+# #     print(i)
+# #     i+=1
+# i=100
+# while i>=1:
+#     print(i)
+#     i-=1
+
+# #print the multipilication table of number n
+# n=int(input("enter an number"))
+# i=1
+# while i <=10:
+#     print(n*i)
+#     i=i+1
+# #print the element of the following list
+
+# nums=[1,4,9,16,25,36,49,64,81,100]
+# i=0
+# # while i< len(nums):
+# #     print(nums[i])
+# #     i+=1
+# # heroes=["ironman","captain amerca","batman"]
+# # i=0
+# # while i<len(heroes):
+# #     print(heroes[i])
+# # #     i+=1
+# # #search for a number x in this tuple using loop
+# # # tup=(1,4,9,16,25,36,49,64,81,100)
+# # # x=36
+# # # i=0
+# # # while i <len(tup):
+# # #     if(tup[i]==x):
+# # #         print("found at index", i)
+# # #     i+=1
+# # #break and continue
+
+# # i=1
+# # while i <= 5:
+# #     print(i)
+# #     if(i==3):
+# #         break
+# #     i+=1
+
+# # print("end of loop")
+
+# # i=0
+# # while i <=5:
+# #     if(i==3):
+# #         i +=1
+# #         continue
+# #     print(i)
+# #     i=i+1
+# num=[1,2,3,4,5]
+# for val in num:
+#     print(val)
+
+# vegetable=["potato","brijal","lady finger"]
+# for val in vegetable:
+#     print(val)
+
+# # list and tuple 2 ta i print kora jay
+
+# srt="apna college"
+# for char in srt:
+#     print(char)
+
+#print the element of the following list using a loop
+# list=[1,4,9,16,25,36,49,64,81,100]
+# for el in list:
+#     print(el)
+
+# tup=(1,4,9,16,25,36,49,64,81,100)
+# x=4
+
+
+# i=0
+# for el in tup:
+#     if(el==x):
+#         print("number found at index ",i)
+    
+#     i=i+1
+
+# range()
+# print(range(5))
+# seq=range(10)
+# for i in seq:
+#     print(i)
+
+#range (start ,end,increment)
+# print(range(1,5))
+
+# for i in range(2,100,2):
+#     print(i)
+#print from 1 to 100
+# for i in range(1,100):
+#     print(i)
+
+
+
+#print 100 to 1
+# for i in range(100,0,-1):
+#     print(i)
+#
+#print a multiplication table of n
+# n=int(input("enter a number :"))
+# for i in range(1,11):
+#     print(n*i)
+
+#pass statement {pass kore chole jai pore abar korar jonno rekhe}
+# for el in range(5):
+#     pass
+
+
+# print("ikram")
+
+#wap to find the sum of first n number using while
+
+# n=int(input("enter a number :"))
+# sum=0
+# i=1
+# while i <=n:
+#     sum=sum+i
+#     i=i+1
+# print(sum)
+# #using for loop 
+# sum=0
+# for i in range(1,n+1):
+#     sum=sum+i
+# print(sum)
+
+#wap to find the factorial of first n number using for
+
+# n=int(input("enter a number :"))
+# fact=1
+# for i in range(1,n+1):
+#     fact=fact*i
+# print(fact)
+
+
+
+
+
+
+
+
+
+
+  
+
+
